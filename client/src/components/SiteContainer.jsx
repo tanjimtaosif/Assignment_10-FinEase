976dca45a85cf0bc41547631ae820c50e23f55fe
tanjimtaosif <itsmahtab007@gmail.com>
2025-11-12 23:35:39 +0600

    Created site container to keep the design consistency

---
+++ client/src/components/SiteContainer.jsx
@@ -0,0 +1,9 @@
+export default function SiteContainer({ children, className = "" }) {
+    return (
+        <div
+            className={`mx-auto w-full max-w-[1440px] px-4 sm:px-6 lg:px-8 ${className}`}
+        >
+            {children}
+        </div>
+    );
+}
