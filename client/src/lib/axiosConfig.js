ef8bf9486109145465a2dbd6f5888aed3ece98d8
tanjimtaosif <itsmahtab007@gmail.com>
2025-11-13 18:43:16 +0600

    Set up the axios configuration

--- client/src/lib/axiosConfig.js
+++ client/src/lib/axiosConfig.js
@@ -1,20 +1,32 @@
+// src/lib/axiosConfig.js
 import axios from "axios";
 import { auth } from "./firebase";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL?.replace(/\/+$/, "") || "",
+    // remove any trailing slashes just in case
+    baseURL:
+        import.meta.env.VITE_API_BASE_URL?.replace(/\/+$/, "") ||
+        "http://localhost:5000",
     timeout: 15000,
-    headers: { "Content-Type": "application/json", Accept: "application/json" },
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+    },
 });
 
+// Attach Firebase ID token (if logged in)
 api.interceptors.request.use(async (config) => {
     const user = auth.currentUser;
+
     if (user) {
         const token = await user.getIdToken();
-        if (token) config.headers.Authorization = `Bearer ${token}`;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
     } else {
         delete config.headers.Authorization;
     }
+
     return config;
 });
 
